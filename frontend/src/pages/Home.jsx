666823a6b3512fd8b13fd0ed64e53927dce0c8bc
M.can <caglayan.mertcan37@gmail.com>
2025-10-11 15:48:05 +0300

    update navigation on quizpage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/home.css";
 import "../styles/global.css";
 import LanguageSelector from "../components/LanguageSelector";
@@ -6,9 +6,28 @@ import CategorySelector from "../components/CategorySelector";
 import GreetingsSection from "../components/GreetingsSection";
 
 function Home() {
-	const [step, setStep] = useState(0);
+	const [step, setStep] = useState(() => {
+		const validSteps = [0, 1, 2];
+		const savedStep = localStorage.getItem("homePageStepVocabvault");
+
+		let parsedStep = null;
+		try {
+			parsedStep = savedStep !== null ? JSON.parse(savedStep) : null;
+		} catch (e) {
+			parsedStep = null;
+		}
+
+		return validSteps.includes(parsedStep) ? parsedStep : 0;
+	});
+
+	useEffect(() => {
+		if (step !== null) {
+			localStorage.setItem("homePageStepVocabvault", JSON.stringify(step));
+		}
+	}, [step]);
 
 	const nextStep = () => setStep((e) => e + 1);
+
 	const prevStep = () => setStep((e) => (e > 0 ? e - 1 : 0));
 	return (
 		<div key={step} className="fade-wrapper">
